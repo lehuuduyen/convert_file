9d938138db4777d7243cbe3028f3f8d10fada3db
nganvu <nganvu215@gmail.com>
2023-07-24 15:03:28 +0700

    create button upload

--- index.php
+++ index.php
@@ -1,13 +1,28 @@
-<?php 
-require_once('routes.php');
-
-?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="/assets/css/style.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script> -->
+    <!-- <link rel="stylesheet" href="/assets/style.css" type="text/css"> -->
+    <title>Demo PHP MVC</title>
+</head>
+
 <body>
-
-<h1>Hello</h1>
-<p>My first paragraph.</p>
-
+    <main style="height: auto !important;">
+        <div class="upload-container">
+            <div class="upload-container-form">
+                <div tabindex="0" class="dropzone">
+                    <input type="file" tabindex="-1" multiple autocomplete="off" style="display: none;">
+                    <div class="btn">
+                        <button class="uk-button uk-button-primary choose-button">Chọn tệp</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </body>
+
 </html>
