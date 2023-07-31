077ce89e3449096dbe37d3ed6a95dacd66c3ac77
ICDVN-DUYENLH\User <duyenlh@icd-vn.com>
2023-07-31 17:00:08 +0700

    update

--- test.php
+++ test.php
@@ -1,50 +1,53 @@
 <?php
 error_reporting(9);
 
-
-// if ($_REQUEST['action'] == 'submit') {
-    
-// }
-try {
-    $_FILES['to'] = '7z';
-
-    $ch = curl_init();
-    // $filePath = $_FILES['file_upl']['tmp_name'];
-    // $fileName = $_FILES['file_upl']['name'];
-    $data = array('file' => "", 'to' => '7z');
+if ($_REQUEST['action'] == 'submit') {
+    try {
+        $ch = curl_init();
+        // $filePath = $_FILES['file_upl']['tmp_name'];
+        // $fileName = $_FILES['file_upl']['name'];
+        $target_format = 'jpg';
+        $total_size = 81295;
+        $source_mime = 'image%2Fpng';
+        $filename = 'duyen.png';
+        $fileInfo = $_FILES['file'];
+        $filePath = $fileInfo['tmp_name'];
+        $fileName = $fileInfo['name'];
+        curl_setopt($ch, CURLOPT_URL, "https://mconverter.eu/cf_nocache/ajax/upload.php?target_format=$target_format&total_size=$total_size&source_mime=$source_mime&filename=$filename&abd=false");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, [
+            'file' => new CURLFile($filePath, 'application/octet-stream', $fileName)
+        ]);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        $info = curl_exec($ch);
+        curl_close($ch);
+        $info = json_decode($info);
    
-    $random = generateRandomString(32);
-    curl_setopt($ch, CURLOPT_URL, 'https://anyconv.com/api/action/add/' . $random);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $_FILES);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    // curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-    $info = curl_exec($ch);
-    // $info = curl_getinfo($ch);
-    echo '<pre>';
-    print_r($info);
-    echo '</pre>';
-} catch (\Throwable $th) {
-    throw $th;
-}
-   
-    die;
-    
-    curl_close($ch);
-function generateRandomString($length = 10)
-{
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        
+        // $info = curl_getinfo($ch);
+        if($info && isset($info->token) && $info->token!=""){
+            $redirect_url = "https://mconverter.eu/cf_nocache/ajax/download.php?token=".$info->token;
+            header("Location: " . $redirect_url);
+        }
+        elseif(isset($info->daily_limit_exceeded) && $info->daily_limit_exceeded ==true){
+            echo "qua gioi han 1 ngay";
+        }else{
+        echo "upload co loi vui long thu lai";
+
+        }
+    } catch (\Throwable $th) {
+        throw $th;
     }
-    return $randomString;
+    die;
 }
+
+
+
 ?>
 
 <form name="file_up" action="" method="POST" enctype="multipart/form-data">
     Upload your file here
-    <input type="file" name="file_upl" id="file_upl" />
+    <input type="file" name="file" id="file_upl" />
     <input type="submit" name="action" value="submit" />
 </form>
